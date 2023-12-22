b5fa20511757ac26e85b729295da7beca32134b9
Phongphisut002 <65160231@go.buu.ac.th>
2023-12-22 10:08:52 +0700

    test

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
 </head>
 <body>
-    <?php echo "HI" ?>
+    <div class="head">
+        <h1>ตารางเเม่สูตรคูณ</h1>
+    </div>
+
     
 </body>
 </html>

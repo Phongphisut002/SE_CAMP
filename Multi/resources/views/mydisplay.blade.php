c2423e71c6ffdee3abccd241181cefeec2e42260
Phongphisut002 <65160231@go.buu.ac.th>
2024-01-06 17:09:36 +0700

    uui

--- Multi/resources/views/mydisplay.blade.php
+++ Multi/resources/views/mydisplay.blade.php
@@ -9,28 +9,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
     <style>
        body {
-            background: url('https://i.pinimg.com/564x/70/89/b8/7089b8f5419717b5b83e0f59b44a8102.jpg');
-            background-size: cover;
-            background-color:#ffffff05);
-            backdrop:blur(10px);
-            -webkit-backdrop-filter:blur(25px);
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin:0;
             font-family: 'Itim', cursive;
         }
-
-        h1 {
-            color: #000000;
-            text-shadow: 3px 3px rgb(121, 118, 118);
-            font-size: 34px;
-            font-weight: bold;
-        }
-
-        div.HW {
+        div.container {
             width:50%;
             max-height: 500px;
             overflow-y: scroll;
@@ -43,7 +24,7 @@
 </head>
 <body>
 
-    <div class="HW">
+    <div class="container">
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $x = $_POST["myinputnumber"];
